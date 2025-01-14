1ac53e47fdbd3a2d33aaee27cfa6940b2f7ba8ae
Md Aftab Uddin <iamaftabuddin123@gmail.com>
2025-01-14 11:36:53 +0530

    Leetcode Problems Day 14

---
+++ src/PrefixSum/_7PrefixCommonArray.java
@@ -0,0 +1,11 @@
+package PrefixSum;
+
+public class _7PrefixCommonArray {
+    public int[] findThePrefixCommonArray(int[] A, int[] B) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
