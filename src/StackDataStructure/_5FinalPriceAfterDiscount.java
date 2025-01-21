c43fcf43e74bd391f763a1c4dda142b40d2323b7
Md Aftab Uddin <iamaftabuddin123@gmail.com>
2025-01-21 22:57:09 +0530

    Leetcode problems Day 21

--- src/StackDataStructure/_5FinalPriceAfterDiscount.java
+++ src/StackDataStructure/_5FinalPriceAfterDiscount.java
@@ -1,7 +1,18 @@
 package StackDataStructure;
 
 public class _5FinalPriceAfterDiscount {
-//    public int[] finalPrices(int[] prices) {
-//
-//    }
+    public int[] finalPrices(int[] prices) {
+        int n = prices.length;
+        int[] result = prices.clone();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (prices[j] <= prices[i]) {
+                    result[i] = prices[i] - prices[j];
+                    break;
+                }
+            }
+        }
+        return result;
+    }
 }
