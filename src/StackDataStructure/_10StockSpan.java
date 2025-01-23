5dc8dc83952b579a3833a2f19c0ec717c73502c8
Md Aftab Uddin <iamaftabuddin123@gmail.com>
2025-01-23 22:21:43 +0530

    Leetcode problem Day 23

---
+++ src/StackDataStructure/_10StockSpan.java
@@ -0,0 +1,18 @@
+package StackDataStructure;
+
+import java.util.Stack;
+
+class StockSpanner {
+    Stack<Integer> st;
+    public StockSpanner() {
+        st = new Stack<>();
+    }
+
+    public int next(int price) {
+        int cnt = 0;
+        return 1;
+    }
+}
+public class _10StockSpan {
+    
+}
--- src/StackDataStructure/_9PreviousGreaterElement.java
+++ src/StackDataStructure/_9PreviousGreaterElement.java
@@ -18,13 +18,30 @@ public class _9PreviousGreaterElement {
             System.out.print(ele+" ");
         }
     }
+    private static void nextGreaterToLeft(int[] arr) {
+        Stack<Integer> st = new Stack<>();
+        int[] ans = new int[arr.length];
+        for(int i=0; i<arr.length; i++) {
+            if(st.isEmpty()) ans[i] = -1;
+            else if(!st.isEmpty() && st.peek()>=arr[i]) ans[i] = st.peek();
+            else if(!st.isEmpty() && st.peek() <=arr[i]){
+                while(!st.isEmpty() && st.peek()<=arr[i]) st.pop();
+                if(st.isEmpty()) ans[i] = -1;
+                else ans[i] = st.peek();
+            }
+            st.push(arr[i]);
+        }
+        for(int ele: ans) {
+            System.out.print(ele+" ");
+        }
+    }
     public static void main(String[] args) {
         int[] arr = {3,10,4,2,1,6,1,7,2,9};
         for(int ele: arr) {
             System.out.print(ele+" ");
         }
         System.out.println();
-        previousGreater(arr);
+        nextGreaterToLeft(arr);
         System.out.println();
     }
 }
