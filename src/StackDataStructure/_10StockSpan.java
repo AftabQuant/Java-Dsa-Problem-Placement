1af48247466a08f3dd577b2109017099742b5fcb
Md Aftab Uddin <iamaftabuddin123@gmail.com>
2025-01-24 15:12:20 +0530

    Leetcode problem Day 24

--- src/StackDataStructure/_10StockSpan.java
+++ src/StackDataStructure/_10StockSpan.java
@@ -1,18 +1,36 @@
 package StackDataStructure;
-
-import java.util.Stack;
+import java.util.*;
 
 class StockSpanner {
     Stack<Integer> st;
+    List<Integer> list;
     public StockSpanner() {
+        list = new ArrayList<>();
         st = new Stack<>();
     }
-
     public int next(int price) {
-        int cnt = 0;
-        return 1;
+        list.add(price);
+        while(!st.isEmpty() && list.get(st.peek())<=price) st.pop();
+        int prev = st.isEmpty()? -1:st.peek();
+        int curr = list.size()-1;
+        int res = curr - prev;
+        st.push(curr);
+        return  res;
     }
 }
 public class _10StockSpan {
-    
+    public ArrayList<Integer> calculateSpan(int[] arr) {
+        Stack<Integer> st = new Stack<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> ans = new ArrayList<>();
+        for(int i=0; i<arr.length; i++) {
+            list.add(arr[i]);
+            while(!st.isEmpty() && list.get(st.peek())<=arr[i]) st.pop();
+            int prev = st.isEmpty() ? -1 : st.peek();
+            int res = i - prev;
+            st.push(i);
+            ans.add(res);
+        }
+        return ans;
+    }
 }
---
+++ src/StackDataStructure/_11LargestRectangleInHist.java
@@ -0,0 +1,14 @@
+package StackDataStructure;
+import java.util.*;
+
+public class _11LargestRectangleInHist {
+    public
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        Stack<Integer> st = new Stack<>();
+        ArrayList<Integer> list1 = new ArrayList<>();
+        for(int i=0; i<n; i++){
+
+        }
+    }
+}
