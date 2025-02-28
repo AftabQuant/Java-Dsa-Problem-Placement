0d26cc002ce9e1dba79c5c522a4b242c5fd8cea7
Md Aftab Uddin <iamaftabuddin123@gmail.com>
2025-02-28 11:06:23 +0530

    stack

--- src/StackDataStructure/_3ValidParentheses.java
+++ src/StackDataStructure/_3ValidParentheses.java
@@ -2,6 +2,17 @@ package StackDataStructure;
 import java.util.*;
 
 public class _3ValidParentheses {
+    public boolean isValid_revision(String s) {
+        Stack<Character> st = new Stack<>();
+        for(int i=0; i<s.length(); i++) {
+            char ch = s.charAt(i);
+            if(ch=='(') st.push(')');
+            else if(ch=='{') st.push('}');
+            else if(ch=='[') st.push(']');
+            else if(st.isEmpty() || st.pop()!=ch) return false;
+        }
+        return st.isEmpty();
+    }
     public boolean isValid(String s) {
         Stack<Character> st = new Stack<>();
         for(int i=0; i<s.length(); i++) {
