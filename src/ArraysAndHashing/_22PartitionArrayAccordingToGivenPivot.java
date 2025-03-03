52fbf20ab6a5cd4ecaa04aa0de48aacd5dda52a9
Md Aftab Uddin <iamaftabuddin123@gmail.com>
2025-03-03 10:26:31 +0530

    Dynamic Programming start

--- src/ArraysAndHashing/_22PartitionArrayAccordingToGivenPivot.java
+++ src/ArraysAndHashing/_22PartitionArrayAccordingToGivenPivot.java
@@ -20,14 +20,14 @@ public class _22PartitionArrayAccordingToGivenPivot {
     public int[] pivotArray(int[] nums, int pivot) {
         int[] result = new int[nums.length];
         int left = 0;
-        for(int i=0; i<nums.length; i++){
-            if(nums[i]<pivot) result[left++] = nums[i];
+        for (int num : nums) {
+            if (num < pivot) result[left++] = num;
         }
-        for(int i=0; i<nums.length; i++){
-            if(nums[i]==pivot) result[left++] = nums[i];
+        for (int num : nums) {
+            if (num == pivot) result[left++] = num;
         }
-        for(int i=0; i<nums.length; i++){
-            if(nums[i]>pivot) result[left++] = nums[i];
+        for (int num : nums) {
+            if (num > pivot) result[left++] = num;
         }
         return result;
     }
