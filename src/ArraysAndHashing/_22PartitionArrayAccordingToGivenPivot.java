178e8160ce531ffcda2b6022a8ab1412ed62dc9b
Md Aftab Uddin <iamaftabuddin123@gmail.com>
2025-03-03 10:15:39 +0530

    Leetcode Problem Solved Day 01

---
+++ src/ArraysAndHashing/_22PartitionArrayAccordingToGivenPivot.java
@@ -0,0 +1,35 @@
+package ArraysAndHashing;
+
+import java.util.*;
+
+public class _22PartitionArrayAccordingToGivenPivot {
+    public int[] pivotArray1(int[] nums, int pivot) {
+        List<Integer> smaller = new ArrayList<>();
+        List<Integer> equal = new ArrayList<>();
+        List<Integer> greater = new ArrayList<>();
+        for(int ele: nums){
+            if(ele<pivot) smaller.add(ele);
+            else if(ele==pivot) equal.add(ele);
+            else greater.add(ele);
+        }
+        for(int i=0; i<smaller.size(); i++) nums[i] = smaller.get(i);
+        for(int i=0; i<equal.size(); i++) nums[i+smaller.size()] = equal.get(i);
+        for(int i=0; i<greater.size(); i++) nums[i+smaller.size()+equal.size()] = greater.get(i);
+        return nums;
+    }
+    public int[] pivotArray(int[] nums, int pivot) {
+        int[] result = new int[nums.length];
+        int left = 0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]<pivot) result[left++] = nums[i];
+        }
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]==pivot) result[left++] = nums[i];
+        }
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]>pivot) result[left++] = nums[i];
+        }
+        return result;
+    }
+}
+
---
+++ src/BasicsPractice.java
@@ -0,0 +1,43 @@
+public class BasicsPractice {
+    public static void print(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (i % 2 != 0 && j % 2 != 0) System.out.print(1 + " ");
+                else if (i % 2 == 0 && j % 2 != 0) System.out.print(0 + " ");
+                else if (i % 2 == 0) System.out.print(1 + " ");
+                else System.out.print(0 + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+    public static void pattern3(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if(i == 1 ||  j == 1 || i == n || j == n ) System.out.print("* ");
+                else System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern1(int n) {
+        for(int i=1; i<=n; i++) {
+            for(int j=n; j>=i; j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static boolean primeNumber(int n){
+        if(n==0 || n==1) return false;
+        for(int i=2; i<n; i++) {
+            if(n%i==0) return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        for(int i=1; i<=100; i++) {
+            if(primeNumber(i)) System.out.print(i + " ");
+        }
+    }
+}
