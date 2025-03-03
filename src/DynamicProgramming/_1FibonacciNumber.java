a2668cd75cd4be55ed7697c7c64f1546f95c3505
Md Aftab Uddin <iamaftabuddin123@gmail.com>
2025-03-03 11:07:18 +0530

    Fibonacci Problem

--- src/DynamicProgramming/_1FibonacciNumber.java
+++ src/DynamicProgramming/_1FibonacciNumber.java
@@ -4,9 +4,7 @@ public class _1FibonacciNumber {
     public static int fibo(int n, int[] dp) {
         if (n==1 || n==0) return n;
         if(dp[n] != 0) return dp[n];
-        int ans = fibo(n-1, dp) + fibo(n-2, dp);
-        dp[n] = ans;
-        return ans;
+        return dp[n] = fibo(n-1, dp) + fibo(n-2, dp);
     }
     public static int fib(int n) {
         int[] dp = new int[n+1];
