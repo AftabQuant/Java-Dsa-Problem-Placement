789bfe9b5fa1e273947434c700a700f2067e4f86
Md Aftab Uddin <iamaftabuddin123@gmail.com>
2025-03-04 23:26:02 +0530

    Leetcode problem Day 02

--- src/DynamicProgramming/_1FibonacciNumber.java
+++ src/DynamicProgramming/_1FibonacciNumber.java
@@ -1,13 +1,14 @@
 package DynamicProgramming;
 
 public class _1FibonacciNumber {
-    public static int fibo(int n, int[] dp) {
-        if (n==1 || n==0) return n;
-        if(dp[n] != 0) return dp[n];
+    public static int fibo(int n, int[] dp){
+        if(n<2) return n;
+        if(dp[n]!=0) return dp[n];
         return dp[n] = fibo(n-1, dp) + fibo(n-2, dp);
     }
     public static int fib(int n) {
         int[] dp = new int[n+1];
         return fibo(n, dp);
     }
+
 }
