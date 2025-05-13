4f168ef78c091ee5dfffe50242de4e713aa020dd
Md Aftab Uddin <iamaftabuddin123@gmail.com>
2025-05-13 10:27:34 +0530

    Leetcode problem solved

--- src/LeetcodeProblems.java
+++ src/LeetcodeProblems.java
@@ -1,6 +1,13 @@
 import java.util.HashMap;
 import java.util.Map;
 
+class ListNode {
+    int val;
+    ListNode next;
+    public ListNode(int val) {
+        this.val = val;
+    }
+}
 public class LeetcodeProblems {
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
         int i=m-1; int j=n-1; int k = m+n-1;
@@ -23,6 +30,25 @@ public class LeetcodeProblems {
             else map.put(nums[i], i);
         }
         return res;
+    }
+    public static ListNode lastNode(ListNode head, int k) {
+        ListNode slow = head;
+        ListNode fast = head;
+        for(int i=0; i<k-1; i++) fast=fast.next;
+        while(fast!=null && fast.next!=null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+    public ListNode swapNodes(ListNode head, int k) {
+        ListNode last = lastNode(head, k);
+        ListNode fast = head;
+        for(int i=0; i<k-1; i++) fast = fast.next;
+        int temp = last.val;
+        last.val = fast.val;
+        fast.val = temp;
+        return head;
     }
     public static void main(String[] args) {
 
