342a54a8d7d37f85ffbd11123b23c3f99043bccc
Md Aftab Uddin <iamaftabuddin123@gmail.com>
2025-05-17 09:19:30 +0530

    Leetcode problems code changed

--- src/LeetcodeProblems.java
+++ src/LeetcodeProblems.java
@@ -50,23 +50,6 @@ public class LeetcodeProblems {
         fast.val = temp;
         return head;
     }
-    public static void swapFunc(int[] arr , int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = arr[i];
-    }
-    public static void rotateArray(int[] nums) {
-        int i=0; int j = nums.length-1;
-        while(i<j) {
-            swapFunc(nums, i, j);
-            i++; j--;
-        }
-    }
     public static void main(String[] args) {
-        int[] nums = {1,2,3,4,5};
-        rotateArray(nums);
-        for(int ele : nums) {
-            System.out.println(ele);
-        }
     }
 }
