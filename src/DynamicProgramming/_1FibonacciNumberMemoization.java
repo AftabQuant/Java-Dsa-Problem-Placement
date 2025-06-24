693889aaca85823bf9aa2e3b547b6d13542f51e9
Md Aftab Uddin <iamaftabuddin123@gmail.com>
2025-06-25 00:17:18 +0530

    LeetCode Problem 2

--- src/DynamicProgramming/_1FibonacciNumberMemoization.java
+++ src/DynamicProgramming/_1FibonacciNumberMemoization.java
@@ -8,12 +8,8 @@ public class _1FibonacciNumberMemoization {
 
     public static int fibo(int n, int[] dp) {
         if(n <= 1) return n;
-        if(dp[n]!=0) {
-            return dp[n];
-        }
-        int ans = fibo(n-1, dp) + fibo(n-2, dp);
-        dp[n] = ans;
-        return ans;
+        if(dp[n]!=0) return dp[n];
+        return dp[n] = fibo(n-1, dp) + fibo(n-2, dp);
     }
     public static void main(String[] args) {
         System.out.println(fib(4));
