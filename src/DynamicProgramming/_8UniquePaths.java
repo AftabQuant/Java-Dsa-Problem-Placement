1cabe09958654d6aa7920be47c7874ad4dcd9f86
Md Aftab Uddin <iamaftabuddin123@gmail.com>
2025-07-11 08:43:13 +0530

    Leetcode Day 16

--- src/DynamicProgramming/_8UniquePaths.java
+++ src/DynamicProgramming/_8UniquePaths.java
@@ -19,17 +19,17 @@ public class _8UniquePaths {
     // Tabulation Method
     public static int uniquePathsTabulation(int m, int n) {
         if(m <= 1 || n <= 1) return 1;
-        int[][] dp = new int[m+1][n+1];
-        Arrays.fill(dp[0], 1);
-        for(int i=0; i<dp.length; i++) dp[i][0] = 1;
-        for(int i=1; i<dp[0].length; i++) {
-            for(int j=i; j<dp.length; j++) {
+        int[][] dp = new int[m][n];
+        for(int i=0; i<m; i++) dp[i][0] = 1;
+        for(int j=1; j<n; j++) dp[0][j] = 1;
+        for(int i=1; i<m; i++) {
+            for(int j=1; j<n; j++) {
                 dp[i][j] = dp[i][j-1] + dp[i-1][j];
             }
         }
         return dp[m-1][n-1];
     }
     public static void main(String[] args) {
-        System.out.println(uniquePaths(4,4));
+        System.out.println(uniquePaths(3,7));
     }
 }
