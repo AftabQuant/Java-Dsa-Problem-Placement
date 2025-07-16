176c76f1535d5482326eb05a50d63bf0f6deebca
Md Aftab Uddin <iamaftabuddin123@gmail.com>
2025-07-16 10:04:35 +0530

    Leetcode Day 20

---
+++ src/LeetCodeProblem/ValidParenthesis.java
@@ -0,0 +1,18 @@
+package LeetCodeProblem;
+
+import java.util.Stack;
+
+public class ValidParenthesis {
+    public static boolean isValid(String s) {
+        if(s.length() <= 1) return false;
+        Stack<Character> st = new Stack<>();
+        for(int i=0; i<s.length(); i++) {
+            char ch = s.charAt(i);
+            if(ch=='(') st.push(')');
+            else if(ch=='{') st.push('}');
+            else if(ch=='[') st.push(']');
+            else if(st.isEmpty() || st.pop() != ch) return false;
+        }
+        return true;
+    }
+}
