8f5274737e1ad732d69d11e97768e97b6c87af9a
Md Aftab Uddin <iamaftabuddin123@gmail.com>
2025-08-09 09:54:48 +0530

    Bitwise operator coding question  done.

--- src/BitManipulation/AllQuestions.java
+++ src/BitManipulation/AllQuestions.java
@@ -10,13 +10,45 @@ public class AllQuestions {
         x = x ^ y;
         System.out.println("Aftab swap: " + x + " and " + y);
     }
-    public static boolean isSet(int number) {
+    public static void iskthSet(int number) {
         int mask = 1 << 3;
-
+        if((number & mask) == 1) System.out.println("the kth bit is set.");
+        else System.out.println("the 3rd bit is not set.");
+    }
+    public static void turnOnKthBit(int num, int k) {
+        int mask = 1 << k;
+        int result = num | mask;
+        System.out.println("After turn on the kth bit: " + result);
+    }
+    public static void turnOffKthBit(int num, int k) {
+        int mask = ~(1 << k);
+        int result = num & mask;
+        System.out.println("After turn off the kth bit: " + result);
+    }
+    public static void toggleKthBit(int num, int k) {
+        int mask = 1 << k;
+        int result = num ^ mask;
+        System.out.println("After toggle the kth bit: " + result);
+    }
+    public static void turnOfRightBit(int num) {
+        int b = num;
+        int k = 0;
+        while(b%2 == 0) {
+            b = b >> 1;
+            k++;
+        }
+        int mask = ~(1 << k);
+        int ans = num & mask;
+        System.out.println("After turn off rightmost bit: " + ans);
     }
     public static void main(String[] args) {
         System.out.println("The power of 2: " + powerOf2(4));
         System.out.println("Befor swap: " + 5 + " and " + 8);
         swapTwoNumber(5,8);
+        iskthSet(91);
+        turnOnKthBit(91, 3);
+        turnOffKthBit(91, 3);
+        toggleKthBit(91, 3);
+        turnOfRightBit(91);
     }
 }
