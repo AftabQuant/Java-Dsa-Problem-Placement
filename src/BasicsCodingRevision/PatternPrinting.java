1358781a240c4255bc3899060e420c14d36f7f58
Md Aftab Uddin <iamaftabuddin123@gmail.com>
2025-08-15 23:54:25 +0530

    Code done

--- src/BasicsCodingRevision/PatternPrinting.java
+++ src/BasicsCodingRevision/PatternPrinting.java
@@ -92,10 +92,25 @@ public class PatternPrinting {
             System.out.println();
         }
     }
+
+    public static void star_plus(int n) {
+        int mid = n/2+1;
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=n; j++) {
+                if(i==mid)System.out.print("* ");
+            }
+
+            for(int j=1; j<n; j++) {
+                if(j==mid) System.out.print("* ");
+                else if(j!=n-1) System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the row: ");
         int n = sc.nextInt();
-        floyd_triangle(n);
+        star_plus(n);
     }
 }
