c7dcc227a78b6863984d24b335d67a6d92f4b7e8
Md Aftab Uddin <iamaftabuddin123@gmail.com>
2025-08-16 08:57:10 +0530

    Code done

--- src/BasicsCodingRevision/PatternPrinting.java
+++ src/BasicsCodingRevision/PatternPrinting.java
@@ -98,11 +98,56 @@ public class PatternPrinting {
         for(int i=1; i<=n; i++) {
             for(int j=1; j<=n; j++) {
                 if(i==mid)System.out.print("* ");
+                else if(j==mid) System.out.print("* ");
+                else System.out.print("  ");
             }
+            System.out.println();
+        }
+    }
 
-            for(int j=1; j<n; j++) {
-                if(j==mid) System.out.print("* ");
-                else if(j!=n-1) System.out.print("  ");
+    public static void star_cross(int n) {
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=n; j++) {
+                if(i==j || i+j == n+1) System.out.print("* ");
+                else System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void binary_triangle(int n) {
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=i; j++) {
+                if(i%2 == 0){
+                    if(j%2==1) System.out.print("0 ");
+                    else System.out.print("1 ");
+                }
+                else {
+                    if(j%2==1) System.out.print("1 ");
+                    else System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void star_triangle_flip(int n) {
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=n; j++) {
+                if(i+j > n) System.out.print("* ");
+                else System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void number_triangle_flip(int n) {
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<n-i+1; j++) {
+                System.out.print("  ");
+            }
+            for(int j=1; j<=i; j++) {
+                System.out.print(j + " ");
             }
             System.out.println();
         }
@@ -111,6 +156,6 @@ public class PatternPrinting {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the row: ");
         int n = sc.nextInt();
-        star_plus(n);
+        number_triangle_flip(n);
     }
 }
