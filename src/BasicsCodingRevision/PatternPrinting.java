4477a2e73a0621ce48bba96acacacad738d24bfd
Md Aftab Uddin <iamaftabuddin123@gmail.com>
2025-08-16 11:51:39 +0530

    Code done

--- src/BasicsCodingRevision/PatternPrinting.java
+++ src/BasicsCodingRevision/PatternPrinting.java
@@ -152,10 +152,103 @@ public class PatternPrinting {
             System.out.println();
         }
     }
+
+    public static void rhombus(int n) {
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<n-i+1; j++) {
+                System.out.print("  ");
+            }
+            for(int j=1; j<=n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void rhombus2(int n) {
+        int nsp = n;
+        int nst = n;
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<nsp; j++) {
+                System.out.print("  ");
+            }
+            for(int j=1; j<=nst; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+            nsp--;
+
+        }
+    }
+
+    public static void star_pyramid(int n) {
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=n-i; j++) {
+                System.out.print("  ");
+            }
+            for(int j=1; j<=2*i-1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void number_pyramid_palindrome(int n) {
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=n-i; j++) {
+                System.out.print("  ");
+            }
+            for(int j=1; j<=i; j++) {
+                System.out.print(j+" ");
+            }
+            for(int j=i-1; j>=1; j--) {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void star_bridge(int n) {
+        for(int i=1; i<=2*n-1; i++) System.out.print("* ");
+        System.out.println();
+
+        for(int i=2; i<=n; i++) {
+            for(int j=1; j<=n-i+1; j++) {
+                System.out.print("* ");
+            }
+            for(int j=1; j<2*i-2; j++) {
+                System.out.print("  ");
+            }
+            for(int j=1; j<=n-i+1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void number_bridge(int n) {
+        for(int i=1; i<=2*n-1; i++) System.out.print(i+" ");
+        System.out.println();
+        n--;
+        for(int i=1; i<=n; i++) {
+            int a = 1;
+            for(int j=1; j<=n-i+1; j++) {
+                System.out.print(a++ + " ");
+            }
+            for(int j=1; j<=2*i-1; j++) {
+                System.out.print("* ");
+                a++;
+            }
+            for(int j=1; j<=n-i+1; j++) {
+                System.out.print(a++ +" ");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the row: ");
         int n = sc.nextInt();
-        number_triangle_flip(n);
+        number_bridge(n);
     }
 }
