9a6da5d1ee8fcc6177471a30f24b6477f76d80c8
Md Aftab Uddin <iamaftabuddin123@gmail.com>
2025-08-28 08:26:00 +0530

    Done code

--- src/BasicsCodingRevision/PatternPrinting.java
+++ src/BasicsCodingRevision/PatternPrinting.java
@@ -341,10 +341,39 @@ public class PatternPrinting {
         }
     }
 
+    public static void star_hourglass2(int n) {
+        int nst = 2*n - 1;
+        int nsp = 0;
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=nsp; j++) {
+                System.out.print("  ");
+            }
+            for(int j=1; j<=nst; j++) {
+                System.out.print("* ");
+            }
+            nsp++;
+            nst -= 2;
+            System.out.println();
+        }
+        nsp = n-2;
+        nst = 3;
+        for(int i=1; i<n; i++) {
+            for(int j=1; j<=nsp; j++) {
+                System.out.print("  ");
+            }
+            for(int j=1; j<=nst; j++) {
+                System.out.print("* ");
+            }
+            nsp--;
+            nst += 2;
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the row: ");
         int n = sc.nextInt();
-        star_hourglass(n);
+        star_hourglass2(n);
     }
 }
--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String s = "565";
-        int n = s.charAt(0) - '0';
-        System.out.println(n);
-    }
-}
