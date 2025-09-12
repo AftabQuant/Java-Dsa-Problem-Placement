f8ac4e7f0eade01e327fb58a087ba53c51684e28
Md Aftab Uddin <iamaftabuddin123@gmail.com>
2025-09-12 20:12:20 +0530

    Binary Search Revision

--- src/BinarySearch/BinaryS.java
+++ src/BinarySearch/BinaryS.java
@@ -11,8 +11,23 @@ public class BinaryS {
         }
         return -1;
     }
+
+    public static void lowerBound(int[] nums, int x) {
+        int lo=0, hi = nums.length-1;
+        int lb = 0;
+        while(lo <= hi) {
+            int mid = lo + (hi-lo)/2;
+            if(nums[mid] >= x) {
+                lb = mid;
+                hi = mid - 1;
+            }
+            else lo = mid + 1;
+        }
+        System.out.println(lb);
+    }
     public static void main(String[] args) {
-        int[] nums = {1,2,3,4,5,6,7,8,9,10};
+        int[] nums = {1,2,3,4,6,7,9,10};
         System.out.println(search(nums, 5));
+        lowerBound(nums, 8);
     }
 }
