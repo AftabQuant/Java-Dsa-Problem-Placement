30364aa92db8383553403d069fe886348ce8c5b1
AftabQuant <iamaftabuddin123@gmail.com>
2026-02-21 09:31:47 +0530

    Revision Done

---
+++ src/PrefixSum/RevisionDay1/PrefixSum.java
@@ -0,0 +1,41 @@
+package PrefixSum.RevisionDay1;
+
+public class PrefixSum {
+    public static void printPrimeNumber(int n){
+        for(int i=2; i<=n; i++) {
+            boolean flag = false;
+            for(int j=2; j<=Math.sqrt(i); j++){
+                if(i%j==0) {
+                    flag = true;
+                    break;
+                }
+            }
+            if(!flag) {
+                System.out.print(i + " ");
+            }
+        }
+
+    }
+
+    public static void isPrimeNumber(int num) {
+        if(num <=1) {
+            System.out.println("Not Prime Number!");
+            return;
+        }
+        for(int i=2; i<=num; i++) {
+            for(int j=2; j<=Math.sqrt(i); j++) {
+                if(i%j == 0){
+                    System.out.println("Not Prime Number!");
+                    return;
+                }
+            }
+        }
+        System.out.println("Prime Number!");
+        return;
+    }
+    static void main(String[] args) {
+        printPrimeNumber(100);
+        System.out.println();
+        isPrimeNumber(11);
+    }
+}
