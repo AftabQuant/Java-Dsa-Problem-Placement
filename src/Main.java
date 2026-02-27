f9acfeaa1eb4fe6abe09c6f015217ff793bef390
AftabQuant <iamaftabuddin123@gmail.com>
2026-02-27 10:00:36 +0530

    Code updated..

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,10 @@ public class Main {
     static void main(String[] args) {
         long num = 0;
         String s = "01234";
+        System.out.println("String to Integer conversation:");
         for(int i=0; i<s.length(); i++) {
             int n = (int) s.charAt(i) - 48;
-            System.out.println(n);
+            System.out.print(n+" ");
         }
     }
 }
